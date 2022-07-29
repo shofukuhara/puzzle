c526a172fc002f0b276253d296625d975983f553
shofukuhara <sho.fukuhara0213@gmail.com>
2022-07-29 14:30:21 +0900

    fix:font修正

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function add_files()
 {
-  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Didact+Gothic&family=Zen+Kaku+Gothic+New:wght@400;500&display=swap');
+  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;500;700&display=swap');
   wp_enqueue_style('style-css', get_template_directory_uri() . '/src/css/style.css');
   wp_enqueue_script('mainjs', get_template_directory_uri() . '/js/main.js');
 };
