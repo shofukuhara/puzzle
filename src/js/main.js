6fc8ef479f663b844c9ecc3a9ad933481c080b57
shofukuhara <sho.fukuhara0213@gmail.com>
2022-08-23 22:12:01 +0900

    fix:スタイル修正

--- src/js/main.js
+++ src/js/main.js
@@ -2,26 +2,32 @@ document.addEventListener(
   "DOMContentLoaded",
   function () {
     const logo = document.querySelector(".header__box-logo");
-    const list = document.querySelectorAll(".header__list-link");
+    const link = document.querySelectorAll(".header__list-link");
     let windowHeight = document.documentElement.clientHeight;
     const hamburger = document.querySelector(".header__hamburger");
     const hamburgerList = document.querySelector(".header__list");
     window.addEventListener("scroll", function () {
       if (this.scrollY > windowHeight) {
         logo.classList.add("scroll");
-        list.forEach((list) => {
-          list.classList.add("scroll");
+        link.forEach((link) => {
+          link.classList.add("scroll");
         });
       } else {
         logo.classList.remove("scroll");
-        list.forEach((list) => {
-          list.classList.remove("scroll");
+        link.forEach((link) => {
+          link.classList.remove("scroll");
         });
       }
     });
     hamburger.addEventListener("click", function () {
       hamburger.classList.toggle("active");
       hamburgerList.classList.toggle("active");
+    });
+    link.forEach((link) => {
+      link.addEventListener("click", () => {
+        hamburgerList.classList.remove("active");
+        hamburger.classList.remove("active");
+      });
     });
   },
   false
